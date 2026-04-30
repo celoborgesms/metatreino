75086050e304104ec3b843f2ad15a252035b5f94
celoborgesms <celoborgesms@gmail.com>
2026-04-30 15:42:09 -0300

    Add files via upload

--- js/version.js
+++ js/version.js
@@ -1,5 +1,5 @@
-window.METATREINO_VERSION = "1.4.5";
-window.METATREINO_VERSION_LABEL = "Versão 1.4.5 Beta";
-window.METATREINO_BUILD = "1.4.5-nivel-pago-estado-inteligente";
-window.METATREINO_CACHE = "metatreino-cache-v1.4.5-nivel-pago-estado-inteligente";
-window.METATREINO_CACHE_NAME = "metatreino-cache-v1.4.5-nivel-pago-estado-inteligente";
+window.METATREINO_VERSION = "1.4.4";
+window.METATREINO_VERSION_LABEL = "Versão 1.4.4 Beta";
+window.METATREINO_BUILD = "1.4.4-perfil-conectado";
+window.METATREINO_CACHE = "metatreino-cache-v1.4.4-perfil-conectado";
+window.METATREINO_CACHE_NAME = "metatreino-cache-v1.4.4-perfil-conectado";
