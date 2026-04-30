75863250b56b1d08efce946334da975d3e05ac50
celoborgesms <celoborgesms@gmail.com>
2026-04-30 17:49:01 -0400

    Delete js directory

--- js/version.js
+++
@@ -1,5 +0,0 @@
-window.METATREINO_VERSION = "1.4.2";
-window.METATREINO_VERSION_LABEL = "Versão 1.4.2 Beta";
-window.METATREINO_BUILD = "1.4.2-nucleo-permanente-divisoes";
-window.METATREINO_CACHE = "metatreino-cache-v1.4.2-nucleo-permanente-divisoes";
-window.METATREINO_CACHE_NAME = "metatreino-cache-v1.4.2-nucleo-permanente-divisoes";
