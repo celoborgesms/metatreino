3331e5cc59ab5488d5431de4c789e6d4479d8e55
celoborgesms <celoborgesms@gmail.com>
2026-07-19 21:50:59 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const APP_VERSION = 'v11.24';
+const APP_VERSION = 'v4.5';
 const CACHE = 'metatreino-' + APP_VERSION;
 const CRITICAL = ['./index.html', './app.js']; // network-first (updates first)
 const STATIC = ['./manifest.json', './icon.svg', './icon-512.png']; // cache-first
